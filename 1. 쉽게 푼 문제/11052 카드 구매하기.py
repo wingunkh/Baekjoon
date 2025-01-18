11c1f562838fc198bfc28a56174ab1f7e0ed0a0b
wingunkh <99gusrms@naver.com>
2025-01-18 21:23:54 +0900

    easy

--- 1. 쉽게 푼 문제/11052 카드 구매하기.py
+++ 1. 쉽게 푼 문제/11052 카드 구매하기.py
@@ -1,8 +1,8 @@
 n = int(input())
 a = [0] + list(map(int, input().split()))
-dp = [0] + [0 for _ in range(n)]
+dp = [0 for _ in range(n + 1)]
+# dp[i] = i개의 카드를 갖기 위해 지불해야 하는 금액의 최댓값
 dp[1] = a[1]
-# dp[i] = i개의 카드를 구매하기 위해 민규가 지불해야 하는 금액의 최댓값
 
 for i in range(2, n + 1):
     for j in range(1, i + 1):
